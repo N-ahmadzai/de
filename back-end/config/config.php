19fcd44829f5aafb308846a18d98d232b1d0a297
N-ahmadzai <167982692+N-ahmadzai@users.noreply.github.com>
2024-07-02 13:00:44 +0200

    Update config.php

--- back-end/config/config.php
+++ back-end/config/config.php
@@ -1,9 +1,10 @@
 <?php
 // Informations de connexion à la base de données
-define('DB_HOST', 'dz8959rne9lumkkw.chr7pe7iynqr.eu-west-1.rds.amazonaws.com');
-define('DB_NAME', 't8qyr2gn5ct50cry');
-define('DB_USER', 'wi45l1tct74bsp3a');
-define('DB_PASS', 'ea84h2c0iornms9i');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'zooarcadia');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
 
 
 
